88b0bdfb615700939d82ad04de65cdee938f3415
abmohamm <mohd.92java@gmail.com>
2020-03-04 20:12:11 -0500

    Added sample service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,14 +15,20 @@ export class HeroesComponent implements OnInit {
   }; */
 
   selectedHero: Hero;
-  heroes: Hero[] = HEROES;
-  constructor() { }
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+      this.getHeroes();
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+      this.heroService.getHeroes()
+                      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
